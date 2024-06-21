0d539cc9ed6c87033eed58885c5be4c3b2bae55d
zzoyu <zzoyu@hotmail.com>
2024-06-22 00:48:47 +0900

    feat: :recycle: add server action to fetch profile

---
+++ app/u/[slug]/actions.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function fetchProfile(slug: string) {
+  const supabase = createClient();
+  const { data, error } = (await supabase
+    .from("profile")
+    .select()
+    .eq("slug", slug)
+    .maybeSingle()) as { data: Profile; error: any };
+
+  return { data, error };
+}
