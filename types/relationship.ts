eb0466f2eb1c69e7487eabd54f383b5df6b6d381
조유진 <yjcho@cowave.kr>
2024-12-24 17:09:06 +0900

    feat: 관계 유형에 아이콘 및 색상 추가

--- types/relationship.ts
+++ types/relationship.ts
@@ -1,6 +1,11 @@
 import { symbol } from "d3";
 import { Character } from "./character";
 
+import SymbolFamily from "@/public/assets/icons/relationship/family.svg";
+import SymbolFriend from "@/public/assets/icons/relationship/friend.svg";
+import SymbolHate from "@/public/assets/icons/relationship/hate.svg";
+import SymbolLove from "@/public/assets/icons/relationship/love.svg";
+
 export interface Relationship {
   id: number;
   name: string;
@@ -17,8 +22,28 @@ export enum ERelationshipType {
 }
 
 export const relationshipTypeList = [
-  { label: "Friend", value: ERelationshipType.FRIEND, symbol: "" },
-  { label: "Family", value: ERelationshipType.FAMILY },
-  { label: "Love", value: ERelationshipType.LOVE },
-  { label: "Hate", value: ERelationshipType.HATE },
+  {
+    label: "Friend",
+    value: ERelationshipType.FRIEND,
+    symbol: SymbolFriend,
+    color: "bg-primary-100",
+  },
+  {
+    label: "Family",
+    value: ERelationshipType.FAMILY,
+    symbol: SymbolFamily,
+    color: "bg-primary-100",
+  },
+  {
+    label: "Love",
+    value: ERelationshipType.LOVE,
+    symbol: SymbolLove,
+    color: "bg-primary-100",
+  },
+  {
+    label: "Hate",
+    value: ERelationshipType.HATE,
+    symbol: SymbolHate,
+    color: "bg-primary-100",
+  },
 ];
