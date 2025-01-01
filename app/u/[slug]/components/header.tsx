7c1b3211a58d78e4e3b5f39388264c2d0f0178d7
조유진 <yjcho@cowave.kr>
2025-01-01 15:57:20 +0900

    feat: 헤더에 로고 클릭 시 홈으로 이동하는 링크 추가

--- app/u/[slug]/components/header.tsx
+++ app/u/[slug]/components/header.tsx
@@ -4,6 +4,7 @@ import { fetchProfileById } from "@/app/profile/actions";
 import { createClient } from "@/utils/supabase/server";
 import { fetchNotificationsByProfileId } from "@/app/notifications/actions";
 import Logo from "@/public/assets/logos/logo_text_horizontal_color.svg";
+import Link from "next/link";
 
 export async function Header() {
   const supabase = createClient();
@@ -15,7 +16,9 @@ export async function Header() {
   return (
     <header className="fixed top-0 flex h-10 md:h-14 w-full items-center justify-between px-2 md:px-8 bg-[#F4F4F4]">
       <div className="flex items-center justify-center">
-        <Logo height={30} />
+        <Link href="/">
+          <Logo height={30} />
+        </Link>
       </div>
       {profile && (
         <ProfileBadge profile={profile} notifications={notifications} />
