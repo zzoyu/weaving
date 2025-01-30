f1fa82df4544b728ffd821d1c07caf03d91d9b23
조유진 <yjcho@cowave.kr>
2025-01-30 20:23:42 +0900

    얼떨결에 해결 #27

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,35 +1,54 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { type CookieOptions, createServerClient } from "@supabase/ssr";
+// The client you created from the Server-Side Auth instructions
+import { createClient } from "@/utils/supabase/server";
+import { fetchProfileById } from "@/app/profile/actions";
+import { createProfile } from "@/app/profile/create/actions";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") || "/profile";
+  const next = searchParams.get("next") ?? "/";
 
   if (code) {
-    const cookieStore = cookies();
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          get(name: string) {
-            return cookieStore.get(name)?.value;
-          },
-          set(name: string, value: string, options: CookieOptions) {
-            cookieStore.set({ name, value, ...options });
-          },
-          remove(name: string, options: CookieOptions) {
-            cookieStore.delete({ name, ...options });
-          },
-        },
-      }
-    );
+    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      const { data: userData, error: userError } =
+        await supabase.auth.getUser();
+      if (userError) {
+        return NextResponse.error();
+      }
+      const profile = await fetchProfileById(userData?.user?.id as string);
+      if (!profile) {
+        const payload = new FormData();
+        payload.append("user_id", userData?.user?.id as string);
+        payload.append(
+          "profile_url",
+          userData?.user?.user_metadata?.picture as string
+        );
+        payload.append(
+          "nickname",
+          userData?.user?.user_metadata?.full_name as string
+        );
+        payload.append(
+          "slug",
+          userData?.user?.user_metadata.user_name as string
+        );
+        await createProfile(payload);
+      }
+
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === "development";
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
     }
   }
 
--- app/components/main-menu.tsx
+++ app/components/main-menu.tsx
@@ -4,31 +4,47 @@ import { createClient } from "@/utils/supabase/client";
 import { User } from "@supabase/supabase-js";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import ProfileImage from "./profile-image";
 import { signInWithTwitter, signOut } from "@/lib/client-authentication";
 
 export default function MainMenu() {
   const supabase = createClient();
 
-  const [isSignedIn, setIsSignedIn] = useState<boolean>();
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
   const [user, setUser] = useState<User>();
 
+  async function handleSignIn() {
+    const { data, error } = await signInWithTwitter();
+    if (error) {
+      console.error(error);
+    }
+    if (data) {
+      console.log(data);
+    }
+  }
+
   useEffect(() => {
-    supabase.auth.onAuthStateChange((_, session) => {
-      if (session) {
-        setIsSignedIn(true);
-        setUser(session.user);
-      } else {
-        setIsSignedIn(false);
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_IN" || event === "USER_UPDATED") {
+        if (session) {
+          setIsSignedIn(true);
+          setUser(session.user);
+          console.log(session.user);
+        } else {
+          setIsSignedIn(false);
+        }
       }
     });
+    return () => {
+      data?.subscription?.unsubscribe?.();
+    };
   }, []);
 
   return (
     <div className="h-1/2 flex flex-col justify-start items-center gap-10 pt-10">
       {isSignedIn === false && (
-        <button onClick={signInWithTwitter} className="text-primary-300">
+        <button onClick={handleSignIn} className="text-primary-300">
           트위터로 시작하기
         </button>
       )}
