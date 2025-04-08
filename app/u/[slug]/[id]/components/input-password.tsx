2d1b1a2acb8dccaa765f57cf08b620faa94d19d2
조유진 <yjcho@cowave.kr>
2025-04-09 00:01:16 +0900

    chore: :lipstick: fix password page #28

--- app/u/[slug]/[id]/components/input-password.tsx
+++ app/u/[slug]/[id]/components/input-password.tsx
@@ -1,19 +1,20 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
 import IconLocked from "@/public/assets/icons/locked.svg";
 
 export default function InputPassword() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen gap-6 p-4 bg-gray-50 rounded-lg shadow-md">
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-6 p-4 rounded-lg ">
       <IconLocked className="w-16 h-16 text-gray-500" />
       <p className="text-lg font-semibold text-gray-700">
         비밀번호를 입력해주세요
       </p>
-      <input
+      <Input
         type="password"
-        name="password"
-        className="w-full max-w-sm p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-primary-200"
         placeholder="비밀번호"
+        className="w-full max-w-sm p-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-primary-200"
+        name="password"
       />
       <button
         type="submit"
