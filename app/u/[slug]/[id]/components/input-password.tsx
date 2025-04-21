a4b18ae5118eba132d2bfd19d9cd6faba38606c7
조유진 <yjcho@cowave.kr>
2025-04-22 02:17:50 +0900

    refactor: 개선된 비밀번호 입력 컴포넌트 스타일 및 구조

--- app/u/[slug]/[id]/components/input-password.tsx
+++ app/u/[slug]/[id]/components/input-password.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
 import IconLocked from "@/public/assets/icons/locked.svg";
 
 export default function InputPassword() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full gap-4">
-      <IconLocked className="w-20 h-20" />
-      <p className="text-xl">비밀번호를 입력해주세요.</p>
-      <input type="password" name="password" />
-      <button type="submit" className="bg-primary-200 text-white p-2 rounded">
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-6 p-4 rounded-lg ">
+      <IconLocked className="w-16 h-16 text-gray-500" />
+      <p className="text-lg font-semibold text-gray-700">
+        비밀번호를 입력해주세요
+      </p>
+      <Input
+        type="password"
+        placeholder="비밀번호"
+        className="w-full max-w-sm p-2 text-gray-700 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-primary-200"
+        name="password"
+      />
+      <button
+        type="submit"
+        className="w-full max-w-sm p-2 text-white bg-primary-100 rounded hover:bg-primary-200 focus:outline-none focus:ring-2 focus:ring-primary-200"
+        style={{ marginTop: "1rem" }}
+      >
         확인
       </button>
     </div>
