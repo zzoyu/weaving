a47b4e835cea20bbc240d0c3cb3126563d64ad53
zzoyu <zzoyu@hotmail.com>
2025-05-06 01:17:55 +0900

    feat(mypage): add logout #42

--- app/mypage/components/mypage-template.tsx
+++ app/mypage/components/mypage-template.tsx
@@ -1,18 +1,28 @@
 "use client";
 
+import { createClient } from "@/utils/supabase/client";
 import ProfileEditForm from "./profile-edit-form";
 
 export default function MypageTemplate({ profile }: { profile: Profile }) {
+  function handleLogout() {
+    const client = createClient();
+    client.auth.signOut().then(() => {
+      window.location.href = "/";
+    });
+  }
+
   return (
     <main className="flex flex-col items-center justify-center w-full h-full">
       <section className="w-full max-w-md p-4 bg-white rounded shadow-md">
-        <h1 className="text-xl font-bold mb-4">마이페이지</h1>
         <div className="mb-4">
           <h2 className="text-lg font-semibold">내 프로필</h2>
           <ProfileEditForm profile={profile} />
         </div>
         <div className="mb-4">
-          <button className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+          <button
+            className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+            onClick={handleLogout}
+          >
             로그아웃
           </button>
         </div>
