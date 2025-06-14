0bec864ed68eac282e51fc31a6821eca0273417e
zzoyu <zzoyu@hotmail.com>
2025-06-15 02:39:41 +0900

    feat: add form validation, authorization #151

--- app/(with-ui)/u/[slug]/add/page.tsx
+++ app/(with-ui)/u/[slug]/add/page.tsx
@@ -1,3 +1,4 @@
+import { fetchPlanByProfileId } from "@/app/actions/plan";
 import { fetchProfileByUserId } from "@/app/profile/actions";
 import { createClient } from "@/utils/supabase/server";
 import CharacterAddTemplate from "./components/template";
@@ -23,11 +24,18 @@ export default async function NewCharacterPage({
     throw new Error("자신의 프로필이 아닙니다.");
   }
 
+  const userPlan = await fetchPlanByProfileId(myProfile.id as number);
+  if (!userPlan) {
+    throw new Error("플랜 정보를 찾을 수 없습니다.");
+  }
+
   return (
     <main className="flex flex-col justify-center items-center pt-10 w-full md:max-w-[40rem] mx-auto gap-10">
-      {myProfile.id && (
-        <CharacterAddTemplate slug={params.slug} profileId={myProfile.id} />
-      )}
+      <CharacterAddTemplate 
+        slug={params.slug} 
+        profileId={myProfile.id as number} 
+        planLimit={userPlan.limit}
+      />
     </main>
   );
 }
