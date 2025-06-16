1c8706797b2ea0caf0e5d32dfb68d5728de2e0c8
zzoyu <zzoyu@hotmail.com>
2025-06-16 22:50:34 +0900

    공지사항 페이지 레이아웃 개선, 제목 텍스트 크기 조정 및 날짜 표시 스타일 변경

--- app/(with-ui)/notifications/notice/[id]/page.tsx
+++ app/(with-ui)/notifications/notice/[id]/page.tsx
@@ -31,23 +31,24 @@ export default async function NoticePage({
   return (
     <main className="flex flex-col justify-start pt-4 md:pt-10 w-full md:max-w-[40rem] mx-auto">
       <div className="fixed top-0 w-full flex p-4">
-        <h2 className="flex gap-2 items-center">
+        <h2 className="flex gap-2 items-center text-xl">
           <ButtonBack />
-          {noticeItem.title}
+          <span className="line-clamp-2">{noticeItem.title}</span>
         </h2>
       </div>
-      <div className="p-4">
-        <p className="text-gray-500">
+      <div className="px-4 py-2">
+        <p className="text-gray-500 text-xs text-right px-4">
+          
           {new Date(noticeItem.created_at).toLocaleDateString("ko-KR", {
             year: "numeric",
             month: "2-digit",
             day: "2-digit",
           })}
         </p>
-        <div className="pt-16 px-4">
+        <div className="pt-6 md:pt-10 px-2 md:px-4">
           <div className="flex flex-col gap-4">
             <article
-              className="prose prose-stone prose-h1:font-pretendard"
+              className="prose prose-stone prose-h1:font-pretendard prose-h1:text-xl prose-h2:text-lg prose-h3:text-base prose-h4:text-sm prose-p:text-base dark:prose-invert"
               dangerouslySetInnerHTML={{ __html: parsedMarkdown }}
             />
           </div>
