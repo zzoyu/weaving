41b2df1f99b5e703a613f6003b399a10b05922f5
zzoyu <zzoyu@hotmail.com>
2025-07-05 17:48:22 +0900

    fix(BUG): :bug: 세계관 페이지 이미지 업로드 안 되던 부분 수정

---
+++ app/(with-ui)/@navigation/u/[slug]/v/page.tsx
@@ -0,0 +1,20 @@
+import { fetchProfileByUserId } from "@/app/profile/actions";
+import { createClient } from "@/utils/supabase/server";
+import {
+  NavigationSignIn,
+  NavigationSignOut,
+} from "../../../components/navigation";
+
+export default async function NavigationPage() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+  const responseProfile = await fetchProfileByUserId(data?.user?.id as string);
+
+  const isSignin = !!data?.user?.id;
+
+  if (!isSignin || !responseProfile || !responseProfile?.slug) {
+    return <NavigationSignOut />;
+  }
+
+  return <NavigationSignIn slug={responseProfile?.slug} />;
+}
