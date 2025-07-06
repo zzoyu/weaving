0a25b04a5ad8796a9e3100e88abb1fcca0d2fa2b
zzoyu <zzoyu@hotmail.com>
2025-07-06 22:57:51 +0900

    feat: 심사용 페이지 구성 변경

---
+++ app/(content-only)/signin/page.tsx
@@ -0,0 +1,39 @@
+import MainMenu from "@/app/components/main-menu";
+import LogoColored from "@/public/assets/logos/logo_text_vertical_color.svg";
+import LogoWhite from "@/public/assets/logos/logo_text_vertical_white.svg";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { redirect?: string };
+}) {
+  const client = createClient();
+  const { data, error } = await client.auth?.getUser?.();
+
+  // 인증된 사용자이고 리다이렉트 URL이 있는 경우
+  if (data?.user?.id && searchParams.redirect) {
+    redirect(searchParams.redirect);
+  }
+
+  // 인증된 사용자이고 리다이렉트 URL이 없는 경우
+  if (data?.user?.id) {
+    redirect("/profile");
+  }
+
+  return (
+    <main className="h-full flex flex-col items-center pt-[10vh]">
+      <div className="flex-1 w-7/12 max-w-40 flex flex-col items-center justify-center gap-2">
+        <span className="block dark:hidden">
+          <LogoColored height={160} />
+        </span>
+        <span className="hidden dark:block">
+          <LogoWhite height={160} />
+        </span>
+      </div>
+
+      <MainMenu />
+    </main>
+  );
+}
