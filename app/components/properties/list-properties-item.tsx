4e5993793093f071823e39cb6c224e932619dc52
zzoyu <zzoyu@hotmail.com>
2025-07-27 18:44:05 +0900

    feat: :sparkles: add input toggle 입력창 에디터 토글 #149

--- app/components/properties/list-properties-item.tsx
+++ app/components/properties/list-properties-item.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Textarea } from "@/components/ui/textarea";
 import DeleteIcon from "@/public/assets/icons/delete.svg";
 import { Property } from "@/types/character";
+import { useEffect, useRef, useState } from "react";
 
 export default function ListPropertiesItem({
   property,
@@ -12,6 +14,26 @@ export default function ListPropertiesItem({
   onChange: (property: Property) => void;
   onDelete: (property: Property) => void;
 }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isExpanded) {
+      textareaRef.current?.focus();
+      textareaRef.current?.setSelectionRange(
+        textareaRef.current.value.length,
+        textareaRef.current.value.length
+      );
+    } else {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(
+        inputRef.current.value.length,
+        inputRef.current.value.length
+      );
+    }
+  }, [isExpanded]);
+
   return (
     <div className="w-full h-fit relative flex items-center justify-center group">
       <div className="w-full flex gap-4">
@@ -28,14 +50,36 @@ export default function ListPropertiesItem({
             onChange({ ...property, key: event.target.value });
           }}
         />
-        <input
-          type="text"
-          className="w-full text-center p-1 border-background-muted focus:outline-none"
-          value={property.value}
-          onChange={(event) => {
-            onChange({ ...property, value: event.target.value });
-          }}
-        />
+        {isExpanded ? (
+          <Textarea
+            ref={textareaRef}
+            className="w-full text-left p-1 border-background-muted focus:outline-none bg-transparent resize-none h-auto overflow-hidden"
+            value={property.value}
+            onChange={(event) => {
+              onChange({ ...property, value: event.target.value });
+              if (event.target.value.length <= 20) {
+                setIsExpanded(false);
+              }
+              if (textareaRef.current) {
+                textareaRef.current.style.height = "auto"; // Reset height to auto to allow expansion
+                textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // Set height to scrollHeight
+              }
+            }}
+          />
+        ) : (
+          <input
+            ref={inputRef}
+            type="text"
+            className="w-full text-center p-1 border-background-muted focus:outline-none"
+            value={property.value}
+            onChange={(event) => {
+              onChange({ ...property, value: event.target.value });
+              if (event.target.value.length > 20) {
+                setIsExpanded(true);
+              }
+            }}
+          />
+        )}
       </div>
       <button
         className="absolute right-0 visible md:invisible md:group-hover:visible"
@@ -48,4 +92,4 @@ export default function ListPropertiesItem({
       </button>
     </div>
   );
-} 
+}
