fb6dabb55b518aade82a00719228ab67e648c556
zzoyu <zzoyu@hotmail.com>
2025-10-14 11:20:38 +0900

    hotfix: 탈퇴 처리 버그 수정 추가로 DB 제약조건 수정하였음

--- app/(with-ui)/mypage/components/button-delete.tsx
+++ app/(with-ui)/mypage/components/button-delete.tsx
@@ -1,15 +1,28 @@
 "use client";
 
 import { AlertDialogConfirm } from "@/components/alert-dialog-confirm";
-import { redirect } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
 import { deleteAccount } from "../actions";
 
 export default function ButtonDelete({ userId }: { userId: string }) {
+  const router = useRouter();
+  const { toast } = useToast();
   return (
     <AlertDialogConfirm
       onConfirm={async () => {
-        await deleteAccount(userId);
-        redirect("/");
+        const { success } = await deleteAccount(userId);
+        if (!success) {
+          return;
+        }
+        toast({
+          title: "회원탈퇴가 완료되었습니다.",
+          description: "이용해주셔서 감사합니다.",
+        });
+        const client = createClient();
+        await client.auth.signOut();
+        router.replace("/");
       }}
       title="정말 탈퇴하시겠습니까?"
       description="탈퇴 후에는 모든 데이터가 삭제됩니다."
