224060daa79fee4f65d5824554fe1cab8c6458d2
zzoyu <zzoyu@hotmail.com>
2025-10-14 23:27:20 +0900

    feat: CSP 헤더에 twimg.com 도메인 추가 및 불필요한 코드 제거

--- next.config.mjs
+++ next.config.mjs
@@ -76,6 +76,7 @@ const nextConfig = {
         "https://www.google-analytics.com",
         "https://*.weavv.in",
         "https://*.oraclecloud.com",
+        "https://*.twimg.com",
         process.env.NEXT_PUBLIC_SUPABASE_URL,
       ],
       "connect-src": [
@@ -87,11 +88,19 @@ const nextConfig = {
         "https://*.adtrafficquality.google",
         "https://fundingchoicesmessages.google.com",
       ],
+      "post-message-src": [
+        "'self'",
+        "https://*.adtrafficquality.google",
+        "https://*.weavv.in",
+      ],
       "frame-src": [
         "https://*.googlesyndication.com",
         "https://googleads.g.doubleclick.net",
         "https://www.googletagmanager.com",
         "https://www.googletagservices.com",
+        "https://*.adtrafficquality.google",
+        "https://*.weavv.in",
+        "https://www.google.com",
       ],
       "font-src": ["https://fonts.gstatic.com", "https://*.weavv.in"],
       "object-src": ["'none'"],
@@ -193,37 +202,6 @@ export default MillionLint.next({
       // https://docs.sentry.io/product/crons/
       // https://vercel.com/docs/cron-jobs
       automaticVercelMonitors: true,
-    }),
-    {
-      // For all available options, see:
-      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-      org: "finding-benjamin",
-      project: "weaving",
-
-      // Only print logs for uploading source maps in CI
-      silent: !process.env.CI,
-
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
-
-      // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-      // This can increase your server load as well as your hosting bill.
-      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-      // side errors will fail.
-      // tunnelRoute: "/monitoring",
-
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-
-      // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
-    }
+    })
   )
 );
