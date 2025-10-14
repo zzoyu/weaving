6426ae9736085fc61539766dc851f775e8a232c7
zzoyu <zzoyu@hotmail.com>
2025-10-14 12:39:10 +0900

    레이아웃에 광고 배너 추가

--- app/(with-ui)/layout.tsx
+++ app/(with-ui)/layout.tsx
@@ -18,7 +18,18 @@ export default async function UILayout({
   return (
     <div className="flex flex-col h-full pt-14">
       {header}
-      <div className="w-full h-full overflow-y-auto">{children}</div>
+      <div className="w-full h-full overflow-y-auto">
+        <div className="px-2 fixed top-14 left-0  z-10 md:w-40 h-full hidden md:block pb-10">
+          <ins
+            className="adsbygoogle justify-center my-2"
+            data-ad-client="ca-pub-8566989289200896"
+            data-ad-slot="5446812182"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+        </div>
+        {children}
+      </div>
       {userId ? navigation : <NavigationSignOut />}
     </div>
   );
