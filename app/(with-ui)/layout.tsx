9daf31baddd6454fb6c6bc356fd91e82238151e8
zzoyu <zzoyu@hotmail.com>
2025-11-03 22:38:14 +0900

    레이아웃 컴포넌트에 광고 스크립트 추가: 광고 표시를 위한 마크업 삽입

--- app/(with-ui)/layout.tsx
+++ app/(with-ui)/layout.tsx
@@ -20,6 +20,15 @@ export default async function UILayout({
     <div className="layout flex flex-col !h-full min-h-full max-h-full pt-14 relative">
       {header}
       <div className="w-full h-full overflow-y-auto grow shrink">
+        <div className="mx-auto">
+          <ins
+            className="adsbygoogle w-full h-full block mx-auto max-w-xl max-h-20"
+            data-ad-client="ca-pub-8566989289200896"
+            data-ad-slot="3734400914"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+        </div>
         {children}
         <div className="px-2 fixed top-14 left-0  z-10 lg:w-40 h-full hidden lg:block pb-10 overflow-y-clip">
           {/* Left ad (desktop only) */}
