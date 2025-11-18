35bfc44afe993ddc3874a1e52f578dc300e6b365
zzoyu <zzoyu@hotmail.com>
2025-11-18 21:26:35 +0900

    헤더 컴포넌트의 프로필 수정 버튼에 아이콘 추가 및 스타일 개선

--- app/(with-ui)/@header/(profile)/u/[slug]/[id]/edit/page.tsx
+++ app/(with-ui)/@header/(profile)/u/[slug]/[id]/edit/page.tsx
@@ -1,11 +1,14 @@
+import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 flex w-full items-center justify-between py-4 px-2 lg:px-8 bg-transparent">
+    <header className="fixed top-0 flex w-full items-center justify-between py-4 px-2 lg:px-8 bg-transparent h-14">
       <Link href="./">
         <button className="p-1 rounded-full h-10 flex items-center justify-center text-2xl  bg-transparent">
-          <span className="text-primary-300 h-fit">← 프로필 수정</span>
+          <span className="text-primary-300 flex flex-row items-center gap-1">
+            <ArrowLeftIcon fill="none" stroke="currentColor" /> 프로필 수정
+          </span>
         </button>
       </Link>
     </header>
